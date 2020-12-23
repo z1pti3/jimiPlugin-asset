259c47c8cdb145049b2c3190c03e83015faa93f8
z1pti3 <z1pti3@outlook.com>
2020-12-23 14:14:46 +0000

    Update

--- asset.py
+++ asset.py
@@ -1,7 +1,7 @@
 from core import plugin, model
 
 class _asset(plugin._plugin):
-    version = 0.5
+    version = 0.6
 
     def install(self):
         # Register models
@@ -9,6 +9,7 @@ class _asset(plugin._plugin):
         model.registerModel("assetUpdate","_assetUpdate","_action","plugins.asset.models.action")
         model.registerModel("assetBulkUpdate","_assetBulkUpdate","_action","plugins.asset.models.action")
         model.registerModel("assetSearch","_assetSearch","_action","plugins.asset.models.assetSearch")
+        model.registerModel("assetSearchTrigger","_assetSearchTrigger","_action","plugins.asset.models.assetSearch")
         model.registerModel("assetRelationship","_assetRelationship","_document","plugins.asset.models.relationship")
         model.registerModel("assetRelationshipUpdate","_assetRelationshipUpdate","_action","plugins.asset.models.relationship")
         model.registerModel("assetRelationshipBulkUpdate","_assetRelationshipBulkUpdate","_action","plugins.asset.models.relationship")
@@ -20,12 +21,15 @@ class _asset(plugin._plugin):
         model.deregisterModel("assetUpdate","_assetUpdate","_action","plugins.asset.models.action")
         model.deregisterModel("assetBulkUpdate","_assetBulkUpdate","_action","plugins.asset.models.action")
         model.deregisterModel("assetSearch","_assetSearch","_action","plugins.asset.models.assetSearch")
+        model.deregisterModel("assetSearchTrigger","_assetSearchTrigger","_action","plugins.asset.models.assetSearch")
         model.deregisterModel("assetRelationship","_assetRelationship","_document","plugins.asset.models.relationship")
         model.deregisterModel("assetRelationshipUpdate","_assetRelationshipUpdate","_action","plugins.asset.models.relationship")
         model.deregisterModel("assetRelationshipBulkUpdate","_assetRelationshipBulkUpdate","_action","plugins.asset.models.relationship")
         return True
     
     def upgrade(self,LatestPluginVersion):
+        if self.version < 0.6:
+            model.registerModel("assetSearchTrigger","_assetSearchTrigger","_action","plugins.asset.models.assetSearch")
         if self.version < 0.5:
             model.registerModel("assetRelationshipBulkUpdate","_assetRelationshipBulkUpdate","_action","plugins.asset.models.relationship")
         if self.version < 0.4:
@@ -35,3 +39,4 @@ class _asset(plugin._plugin):
             model.registerModel("assetBulkUpdate","_assetBulkUpdate","_action","plugins.asset.models.action")
         if self.version < 0.2:
             model.registerModel("assetSearch","_assetSearch","_action","plugins.asset.models.assetSearch")
+        return True
