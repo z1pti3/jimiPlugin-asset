dd92916381b32509e0e572dc1b6a1592622aeb97
z1pti3 <z1pti3@outlook.com>
2021-04-08 08:14:41 +0100

    Started work

--- web/asset.py
+++ web/asset.py
@@ -8,6 +8,8 @@ from plugins.asset.models import asset, relationship
 
 from core import api, helpers, db
 
+import jimi
+
 
 pluginPages = Blueprint('assetPages', __name__, template_folder="templates")
 
@@ -17,7 +19,7 @@ def mainAssetPage():
 	foundAsset = []
 	#for assetObject in assets:
 	#    foundAsset.append({ "_id" : assetObject._id, "name" : assetObject.name, "type" : assetObject.assetType, "lastSeen" : assetObject.lastSeen })
-	return render_template("asset.html",asset=foundAsset)
+	return render_template("asset.html",CSRF=jimi.api.g.sessionData["CSRF"])
 
 @pluginPages.route("/relationship/")
 def getAssetRelationshipPage():
@@ -34,4 +36,151 @@ def getRelationshipsByTimespan(fromAsset,timespan):
 
 	return { "results" : graph }, 200
 
+@pluginPages.route("/radar/",methods=["POST"])
+def radar():
+	result = { "labels" : [], "datasets" : [], "updates" : [], "deletes" : [] }
+	data = json.loads(jimi.api.request.data)
+	labels = ['Sophos','AD','FortiClient','Snow','Humio','Glpi']
+	if labels != data["labels"]:
+		result["labels"] = labels
+	
+	datasets = { 
+		"Assets" : {
+			"label": 'Assets',
+			"data": [65, 59, 90, 81, 56, 10],
+			"fill": True,
+			"backgroundColor": 'rgba(255, 99, 132, 0.2)',
+			"borderColor": 'rgb(255, 99, 132)',
+			"pointBackgroundColor": 'rgb(255, 99, 132)'
+		}
+	}
 
+	for index, value in enumerate(data["datasets"]):
+		if value["label"] not in datasets:
+			result["deletes"].append({ "index" : index })
+		else:
+			if value["data"] != datasets[value["label"]]["data"]:
+				result["updates"].append({ "index" : index, "data" : datasets[value["label"]]["data"] })
+			del datasets[value["label"]]
+	
+	for dataset in datasets:
+		result["datasets"].append(datasets[dataset])
+
+	return result, 200
+
+@pluginPages.route("/doughnut/",methods=["POST"])
+def doughnut():
+	result = { "labels" : [], "datasets" : [], "updates" : [], "deletes" : [] }
+	data = json.loads(jimi.api.request.data)
+	labels = ['Sophos','AD','FortiClient','Snow','Humio','Glpi']
+	if labels != data["labels"]:
+		result["labels"] = labels
+	
+	datasets = { 
+		"OS" : {
+				"label": 'OS',
+				"data": [65, 59, 90, 81, 56, 55],
+				"backgroundColor": ['red',"blue","green","yellow","orange","white"],
+		}
+	}
+
+	for index, value in enumerate(data["datasets"]):
+		if value["label"] not in datasets:
+			result["deletes"].append({ "index" : index })
+		else:
+			if value["data"] != datasets[value["label"]]["data"]:
+				result["updates"].append({ "index" : index, "data" : datasets[value["label"]]["data"] })
+			del datasets[value["label"]]
+	
+	for dataset in datasets:
+		result["datasets"].append(datasets[dataset])
+
+	return result, 200
+
+@pluginPages.route("/pie/",methods=["POST"])
+def pie():
+	result = { "labels" : [], "datasets" : [], "updates" : [], "deletes" : [] }
+	data = json.loads(jimi.api.request.data)
+	labels = ['Sophos','AD','FortiClient','Snow','Humio','Glpi']
+	if labels != data["labels"]:
+		result["labels"] = labels
+	
+	datasets = { 
+		"OS" : {
+				"label": 'OS',
+				"data": [65, 59, 90, 81, 56, 55],
+				"backgroundColor": ['red',"blue","green","yellow","orange","white"],
+		}
+	}
+
+	for index, value in enumerate(data["datasets"]):
+		if value["label"] not in datasets:
+			result["deletes"].append({ "index" : index })
+		else:
+			if value["data"] != datasets[value["label"]]["data"]:
+				result["updates"].append({ "index" : index, "data" : datasets[value["label"]]["data"] })
+			del datasets[value["label"]]
+	
+	for dataset in datasets:
+		result["datasets"].append(datasets[dataset])
+
+	return result, 200
+
+@pluginPages.route("/bar/",methods=["POST"])
+def bar():
+	result = { "labels" : [], "datasets" : [], "updates" : [], "deletes" : [] }
+	data = json.loads(jimi.api.request.data)
+	labels = ['Sophos','AD','FortiClient','Snow','Humio','Glpi']
+	if labels != data["labels"]:
+		result["labels"] = labels
+	
+	datasets = { 
+		"OS" : {
+				"label": 'OS',
+				"data": [65, 59, 90],
+				"backgroundColor": ['red'],
+		}
+	}
+
+	for index, value in enumerate(data["datasets"]):
+		if value["label"] not in datasets:
+			result["deletes"].append({ "index" : index })
+		else:
+			if value["data"] != datasets[value["label"]]["data"]:
+				result["updates"].append({ "index" : index, "data" : datasets[value["label"]]["data"] })
+			del datasets[value["label"]]
+	
+	for dataset in datasets:
+		result["datasets"].append(datasets[dataset])
+
+	return result, 200
+
+
+@pluginPages.route("/line/",methods=["POST"])
+def line():
+	result = { "labels" : [], "datasets" : [], "updates" : [], "deletes" : [] }
+	data = json.loads(jimi.api.request.data)
+	labels = ['Sophos','AD','FortiClient','Snow','Humio','Glpi']
+	if labels != data["labels"]:
+		result["labels"] = labels
+	
+	datasets = { 
+		"OS" : {
+				"label": 'OS',
+				"data": [65, 59, 90],
+				"backgroundColor": ['red'],
+		}
+	}
+
+	for index, value in enumerate(data["datasets"]):
+		if value["label"] not in datasets:
+			result["deletes"].append({ "index" : index })
+		else:
+			if value["data"] != datasets[value["label"]]["data"]:
+				result["updates"].append({ "index" : index, "data" : datasets[value["label"]]["data"] })
+			del datasets[value["label"]]
+	
+	for dataset in datasets:
+		result["datasets"].append(datasets[dataset])
+
+	return result, 200
